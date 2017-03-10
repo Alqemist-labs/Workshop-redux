f6354362703e9863569e9b14583a02070d96af2b
Clément Oriol <clement@alqemist.com>
2017-03-10 10:04:10 +0100

    Remove reducers from exercice 02

--- 02-redux-advanced/src/reducers.js
+++ 02-redux-advanced/src/reducers.js
@@ -2,8 +2,8 @@ import { v4 as uuid } from 'uuid';
 
 /*
  state = {
- todos: [], // Géré todosReducer
- visibility_filter: 'SHOW_ALL' || 'SHOW_ACTIVE' || 'SHOW_COMPLETED',  // Géré par visibilityFilterReducer
+  todos: [], // Géré todosReducer
+  visibility_filter: 'SHOW_ALL' || 'SHOW_ACTIVE' || 'SHOW_COMPLETED',  // Géré par visibilityFilterReducer
  }
 
  Actions :
@@ -13,54 +13,12 @@ import { v4 as uuid } from 'uuid';
  'SET_VISIBILITY_FILTER',
  */
 
-export const todoReducer = (state = {}, action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return {
-        text: action.text,
-        id: uuid(),
-        completed: false,
-      };
-    case 'TOGGLE_TODO':
-      return {
-        ...state,
-        completed: !state.completed,
-      };
-    default:
-      return state;
-  }
+const todoReducer = () => {
 };
 
-export const todosReducer = (state = [], action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todoReducer(undefined, action),
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id === action.id) {
-          return todoReducer(todo, action);
-        } else {
-          return todo;
-        }
-      });
-    case 'REMOVE_TODO':
-      return state.filter(todo => {
-        return todo.id !== action.id;
-      });
-    default:
-      return state
-  }
+export const todosReducer = () => {
 };
 
-export const visibilityFilterReducer = (state = 'SHOW_ALL', action) => {
-  switch(action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  }
+export const visibilityFilterReducer = () => {
 };
 
